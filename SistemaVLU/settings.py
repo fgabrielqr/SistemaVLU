0fa3eaed1b291b12dbad247b7ef5c47f4b7c6876
fgabrielqr <fgabrielqr@gmail.com>
2021-02-01 23:25:41 -0300

    Configurando Banco com PostgreSQL

--- SistemaVLU/settings.py
+++ SistemaVLU/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SistemaVLU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SVLU',
+        'USER': 'postgre',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
