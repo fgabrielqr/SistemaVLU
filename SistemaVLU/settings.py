40ee6848a223837b45d28d17ec17721f9cf3b874
Felipe Gabriel <fgabrielqr@gmail.com>
2021-02-02 22:41:34 -0300

    Inserindo os Models

--- SistemaVLU/settings.py
+++ SistemaVLU/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'myApp.apps.MyappConfig',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
