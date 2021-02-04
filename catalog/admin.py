a9de074bf60d98359d3b2961a86600ca77eee489
Felipe Gabriel <fgabrielqr@gmail.com>
2021-02-03 21:20:46 -0300

    Listando Categorias

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,13 +6,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'created', 'modified']
     search_fields = ['name', 'slug']
-
+    list_filter = ['created', 'modified']
 
 class BookAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'category', 'created', 'modified']
     search_fields = ['name', 'slug', 'category__name']
-
+    list_filter = ['created', 'modified']
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Book, BookAdmin)
