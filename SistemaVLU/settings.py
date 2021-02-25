a51fe61474999f05d6ee24d287e326c741fc3c46
Felipe Gabriel <fgabrielqr@gmail.com>
2021-02-25 10:54:57 -0300

    Configurando Heroku

--- SistemaVLU/settings.py
+++ SistemaVLU/settings.py
@@ -141,6 +141,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
 
