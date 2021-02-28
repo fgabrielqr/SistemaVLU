9cc8c685b99473a285f5ac82213b2050e3a28483
Felipe Gabriel <fgabrielqr@gmail.com>
2021-02-28 14:18:26 -0300

    Adicionando sistema no  Heroku e Secret Key

--- SistemaVLU/settings.py
+++ SistemaVLU/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'znrnio86c(smz^xhik&q0(b=svy8n$xl+mh&g)(_8uh&1w*79='
+SECRET_KEY = os.getenv('SECRET_KEY', '123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
